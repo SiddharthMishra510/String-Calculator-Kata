de4d00d5f43719ae659d392ce499bdfc3fce553f
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-01-16 19:54:30 +0530

    test: add with empty string input

--- Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
+++ Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
@@ -1,6 +1,14 @@
+using NUnit.Framework;
+
 namespace StringCalcProj.Tests;
 
 public class StringCalculatorTests
 {
-    
+    [Test]
+    public void Add_Nothing_ShouldReturnZero()
+    {
+        StringCalculator stringCalculator = new();
+        string result = stringCalculator.Add("");
+        Assert.That(result, Is.EqualTo(0));
+    }
 }
