dc3b41f3a1fe339f43019719f69951b7c798d55e
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-01-16 20:16:03 +0530

    test: test adding one

--- Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
+++ Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
@@ -11,4 +11,12 @@ public class StringCalculatorTests
         int result = stringCalculator.Add("");
         Assert.That(result, Is.EqualTo(0));
     }
+
+    [Test]
+    public void Add_One_ShouldReturnOne()
+    {
+        StringCalculator stringCalculator = new();
+        int result = stringCalculator.Add("1");
+        Assert.That(result, Is.EqualTo(1));
+    }
 }
