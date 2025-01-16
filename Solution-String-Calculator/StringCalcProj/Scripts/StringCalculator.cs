634030e6ffb6f7f5733e762583153e4cb90220cb
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-01-17 02:39:33 +0530

    chore: move StringCalculator class in appropriate directory and remove namespacing

--- Solution-String-Calculator/StringCalcProj/Scripts/StringCalculator.cs
+++ Solution-String-Calculator/StringCalcProj/Scripts/StringCalculator.cs
@@ -1,1 +1,39 @@
-﻿
+﻿public class StringCalculator
+{
+    public int Add(string input)
+    {
+        List<string> tokens = Tokenize(input);
+        List<int> numbers = ParseIntoIntegers(tokens);
+        return Add(numbers);
+    }
+
+    private List<string> Tokenize(string input)
+    {
+        char[] delimiters = { ',', '\n'};
+        string[] tokens = input.Split(delimiters);
+        return tokens.ToList();
+    }
+    
+    private List<int> ParseIntoIntegers(List<string> tokens)
+    {
+        List<int> numbers = new List<int>();
+        foreach (string token in tokens)
+        {
+            if (int.TryParse(token, out int result)) 
+            {
+                numbers.Add(result);
+            }
+        }
+        return numbers;
+    }
+
+    private int Add(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+}
--- Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
+++ Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
@@ -1,7 +1,5 @@
 using NUnit.Framework;
 
-namespace StringCalcProj.Tests;
-
 public class StringCalculatorTests
 {
     [Test]
