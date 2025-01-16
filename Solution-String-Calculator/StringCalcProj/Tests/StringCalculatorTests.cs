90fd55c24e6fa7b4e7cbe764f371ce9388401d48
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-01-17 02:17:43 +0530

    test: add ten numbers

--- Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
+++ Solution-String-Calculator/StringCalcProj/Tests/StringCalculatorTests.cs
@@ -35,4 +35,12 @@ public class StringCalculatorTests
         int result = stringCalculator.Add("1,5,7");
         Assert.That(result, Is.EqualTo(13));
     }
+    
+    [Test]
+    public void Add_TenNumbers_ShouldReturnSum()
+    {
+        StringCalculator stringCalculator = new();
+        int result = stringCalculator.Add("1,5,7,91,0,4,5,67,44,9000");
+        Assert.That(result, Is.EqualTo(9224));
+    }
 }
