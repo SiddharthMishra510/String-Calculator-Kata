90213a2c3f5737ec71d95f33bc5511b9a17de98c
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-01-16 23:25:26 +0530

    refactor: extract single-responsibility methods out of the add method

--- Solution-String-Calculator/StringCalcProj/Tests/StringCalculator.cs
+++ Solution-String-Calculator/StringCalcProj/Tests/StringCalculator.cs
@@ -2,16 +2,42 @@ namespace StringCalcProj.Tests;
 
 public class StringCalculator
 {
-    public int Add(string numbers)
+    public int Add(string input)
     {
-        var listOfNumbers = numbers.Split(",").ToList();
-        int sum = 0;
-        foreach (var number in listOfNumbers)
+        List<string> tokens = Tokenize(input);
+        List<int> numbers = ParseIntoIntegers(tokens);
+        return Add(numbers);
+    }
+
+    private static List<string> Tokenize(string input)
+    {
+        string delimiter = ",";
+        string[] tokens = input.Split(delimiter);
+        return tokens.ToList();
+    }
+    
+    private static List<int> ParseIntoIntegers(List<string> tokens)
+    {
+        List<int> numbers = new List<int>();
+
+        foreach (string token in tokens)
         {
-            if (int.TryParse(number, out var result))
+            if (int.TryParse(token, out int result))
             {
-                sum += result;
+                numbers.Add(result);
             }
+        }
+
+        return numbers;
+    }
+
+    private int Add(List<int> numbers)
+    {
+        int sum = 0;
+        
+        foreach (int number in numbers)
+        {
+            sum += number;
         }
 
         return sum;
