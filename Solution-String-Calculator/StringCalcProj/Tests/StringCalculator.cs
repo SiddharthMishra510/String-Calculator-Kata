231925d7d50c34ce3987e4e65de0bed2555447db
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-01-17 02:19:06 +0530

    refactor: remove extra blank lines

--- Solution-String-Calculator/StringCalcProj/Tests/StringCalculator.cs
+++ Solution-String-Calculator/StringCalcProj/Tests/StringCalculator.cs
@@ -19,27 +19,23 @@ public class StringCalculator
     private static List<int> ParseIntoIntegers(List<string> tokens)
     {
         List<int> numbers = new List<int>();
-
         foreach (string token in tokens)
         {
-            if (int.TryParse(token, out int result))
+            if (int.TryParse(token, out int result)) 
             {
                 numbers.Add(result);
             }
         }
-
         return numbers;
     }
 
     private int Add(List<int> numbers)
     {
         int sum = 0;
-        
         foreach (int number in numbers)
         {
             sum += number;
         }
-
         return sum;
     }
 }
